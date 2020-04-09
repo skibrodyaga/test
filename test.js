9af2e642b9892c247a0177abadc985881500d52e
AndreyS <conga@ukr.net>
2020-04-09 11:53:32 +0300

    my first commit

---
+++ test.js
@@ -0,0 +1,7 @@
+// let al = alert('hey!');
+// console.log(al);
+// let con=confirm('are you here?');
+// console.log(con);
+// let prt=prompt('are you a man?', 'yes))');
+// console.log(prt);
+console.log('hello '+3)
